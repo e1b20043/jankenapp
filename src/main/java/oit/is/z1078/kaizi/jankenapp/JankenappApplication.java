69f65e060c80608d7ceab0a1fe5ee15a36254935
e1b20043 <e1b20043@oit.ac.jp>
2022-10-10 14:24:08 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/z1078/kaizi/jankenapp/JankenappApplication.java
@@ -0,0 +1,13 @@
+package oit.is.z1078.kaizi.jankenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankenappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/z1078/kaizi/jankenapp/JankenappApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1078.kaizi.jankenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
